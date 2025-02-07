703e3eac955a94bddc1e9cc1dd37da87350dcda0
Eouls <ckrlcks00@gmail.com>
2025-02-07 09:48:43 +0900

    [SWEP-81] 앨범에 사진 이동 구현

--- app/src/main/java/com/umc/sweepic/domain/model/sweep/AlbumList.kt
+++ app/src/main/java/com/umc/sweepic/domain/model/sweep/AlbumList.kt
@@ -3,5 +3,6 @@ package com.umc.sweepic.domain.model.sweep
 data class AlbumList(
     val name: String,
     val id: String, // 앨범 ID
-    val uri: String
+    val uri: String,
+    val relativePath: String? = null  // 경로 정보
 )
