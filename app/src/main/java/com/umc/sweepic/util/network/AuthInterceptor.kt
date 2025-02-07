6fdf989fc17085a06f830c61adda3bba88d21c75
JONNY <155631001+INJERMI@users.noreply.github.com>
2025-02-07 17:15:36 +0900

    [SWEP-76] challenge location_logic/test api

---
+++ app/src/main/java/com/umc/sweepic/util/network/AuthInterceptor.kt
@@ -0,0 +1,16 @@
+package com.umc.sweepic.util.network
+
+import android.content.SharedPreferences
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val spf: SharedPreferences
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val authRequest = chain.request().newBuilder().addHeader("Cookie", "connect.sid= ${spf.getString("cookie", "s%3ACni--i6VN30lnlMSCvt46gOEyx6NMl7H.nO6yvfpDYjPd7vJaKgxuQ2dAhhcJWDL2xr6rwWMQtZ8")}").build()
+        return chain.proceed(authRequest)
+    }
+}
