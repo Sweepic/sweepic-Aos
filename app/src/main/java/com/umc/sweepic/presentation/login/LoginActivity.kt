7279133c0814899df839b20e4626d2cda340f1b8
Eouls <ckrlcks00@gmail.com>
2025-02-19 02:10:02 +0900

    [SWEP-102] 웹뷰

--- app/src/main/java/com/umc/sweepic/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/umc/sweepic/presentation/login/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.umc.sweepic.presentation.login
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
@@ -28,7 +33,9 @@ class LoginActivity : AppCompatActivity() {
     private fun initObservers() {
         loginViewModel.loginUrl.observe(this) { url ->
             Log.d("LoginActivity", "카카오 로그인 URL: $url")
-            openKakaoLoginWithCustomTabs(url)
+            if (url.isNotEmpty()) {
+                openKakaoLoginWithWebView(url)
+            }
         }
 
         loginViewModel.sessionId.observe(this) { sessionId ->
@@ -46,6 +53,35 @@ class LoginActivity : AppCompatActivity() {
         binding.kakaoLoginLayout.setOnClickListener {
             loginViewModel.fetchKakaoLoginUrl()
         }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun openKakaoLoginWithWebView(url: String) {
+        // WebView를 보이게 함
+        binding.webView.visibility = View.VISIBLE
+
+        // WebView 설정
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                // 쿠키 매니저를 통해 쿠키 읽기
+                val cookieManager = CookieManager.getInstance()
+                val cookies = cookieManager.getCookie(url)
+                Log.d("LoginActivity", "쿠키: $cookies")
+                // 쿠키에서 "SESSION_ID" 값을 추출
+                cookies?.split(";")?.map { it.trim() }?.find { it.startsWith("SESSION_ID=") }?.let { sessionCookie ->
+                    val sessionId = sessionCookie.substringAfter("=")
+                    if (sessionId.isNotEmpty()) {
+                        Log.d("LoginActivity", "세션 ID: $sessionId")
+                        // 세션 ID 저장 후 다음 화면으로 이동
+                        saveSessionId(sessionId)
+                        navigateToStep1()
+                    }
+                }
+            }
+        }
+        binding.webView.loadUrl(url)
     }
 
     private fun openKakaoLoginWithCustomTabs(url: String) {
